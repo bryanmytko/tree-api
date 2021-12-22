9461ded2931edc666415203925cec8a9e416aa6c
Bryan Mytko <bryanmytko@gmail.com>
2021-12-22 01:03:29 -0500

    Change auth to use async/await

--- routes/auth.js
+++ routes/auth.js
@@ -10,35 +10,36 @@ const middleware = require('../middleware');
 const ROUNDS = 10;
 const { TOKEN_SECRET } = process.env;
 
-const invalidLogin = () => res.status(400).json({ error: 'Invalid login.' });
+const invalidLogin = (res) => res.status(400).json({ error: 'Invalid login.' });
 
 router.post('/signup', (req, res) => {
   const { email, password } = req.body;
 
-  bcrypt.hash(password, ROUNDS, (error, hash) => {
+  bcrypt.hash(password, ROUNDS, async (error, hash) => {
     if(error) return res.status(500).json(error);
 
     const newUser = User({ email, password: hash });
-    newUser.save()
-      .then(user => res.status(200).json({ token: generateToken(user.toObject()) }))
-      .catch(err => res.status(500).json({ err }))
+    const user = await newUser.save().catch(error => {
+      return res.status(500).json({ error });
+    });
+
+    return res.status(200).json({ token: generateToken(user.toObject()) });
   });
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
-  const user = User.findOne({ email })
-    .lean()
-    .then(user => {
-      if(!user) return invalidLogin()
-      bcrypt.compare(password, user.password, (error, match) => {
-        if(error) return res.status(500).json({ error });
-        if(match) return res.status(200).json({ token: generateToken(user) })
+  const user = await User.findOne({ email }).lean();
 
-        return invalidLogin()
-      });
-    });
+  if(!user) return invalidLogin();
+
+  bcrypt.compare(password, user.password, (error, match) => {
+    if(error) return res.status(500).json({ error });
+    if(match) return res.status(200).json({ token: generateToken(user) })
+
+    return invalidLogin(res);
+  });
 });
 
 router.get('/verify-token', middleware.verify, (req, res) => {
