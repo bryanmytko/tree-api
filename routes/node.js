26fb934076fc58ee2aeb92b8b13bead6990b6577
Bryan Mytko <bryanmytko@gmail.com>
2022-01-18 18:15:26 -0500

    Add delete node route

--- routes/node.js
+++ routes/node.js
@@ -6,6 +6,50 @@ const Node = require('../models/node');
 const User = require('../models/user');
 const middleware = require('../middleware');
 
+const recursivelyFindChildren = (obj, searchKey, results = []) => {
+  const r = results;
+
+  Object.keys(obj).forEach(key => {
+    const value = obj[key];
+    if(key === searchKey){
+      r.push(value.toString());
+    } else if(key === 'children' && obj[key].length){
+      value.forEach(node => recursivelyFindChildren(node, searchKey, r));
+    }
+  });
+
+  return r;
+};
+
+router.get('/', middleware.verify, async (req, res) => {
+  const { user } = req.body;
+  const nodes = await Node.find({ user: user._id, parent: null });
+
+  return res.status(200).json({ nodes });
+});
+
+router.get('/children/:id', middleware.verify, async (req, res) => {
+  const { id } = req.params;
+
+  /* Our Node schema has a middleware hook to recursively populate children */
+  const nodes = await Node.findOne({ _id: id });
+
+  return res.status(200).json({ nodes });
+});
+
+router.get('/:id', middleware.verify, async (req, res) => {
+  const { id } = req.params;
+  const node = await Node.findById(id)
+    .populate({
+      path: 'children',
+      model: 'Node'
+    });
+
+  if(!node) return res.status(404).json({ error: 'Not found.' });
+
+  return res.status(200).json({ node });
+});
+
 router.post('/create', middleware.verify, async (req, res) => {
   const { user, title, payload, parentId = null } = req.body;
 
@@ -34,33 +78,17 @@ router.post('/create', middleware.verify, async (req, res) => {
   }
 });
 
-router.get('/:id', middleware.verify, async (req, res) => {
+router.delete('/delete/:id', middleware.verify, async (req, res)  => {
   const { id } = req.params;
-  const node = await Node.findById(id)
-    .populate({
-      path: 'children',
-      model: 'Node'
-    });
-
-  if(!node) return res.status(404).json({ error: 'Not found.' });
-
-  return res.status(200).json({ node });
-});
-
-router.get('/children/:id', middleware.verify, async (req, res) => {
-  const { id } = req.params;
-
-  /* Our Node schema has a middleware hook to recursively populate children */
   const nodes = await Node.findOne({ _id: id });
+  const nodeIds = recursivelyFindChildren(nodes.toObject(), '_id');
 
-  return res.status(200).json({ nodes });
-});
-
-router.get('/', middleware.verify, async (req, res) => {
-  const { user } = req.body;
-  const nodes = await Node.find({ user: user._id, parent: null });
-
-  return res.status(200).json({ nodes });
+  try {
+    await Node.deleteMany({ _id: { $in: nodeIds }});
+    return res.status(200).json({});
+  } catch(err) {
+    return res.status(500),json({ err });
+  }
 });
 
 module.exports = router;
