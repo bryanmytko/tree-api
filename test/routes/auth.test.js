7902ba4fc4c0005533aef7724a61a9387e39a174
Bryan Mytko <bryanmytko@gmail.com>
2022-01-28 05:58:50 -0500

    Add login test

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -34,6 +34,49 @@ describe('POST /api/auth/signup', () => {
 });
 
 describe('POST /api/auth/login', () => {
-  it.todo('should log the user in');
-  it.todo('should return an error if the user is not found.');
+  const params = { email: 'foo@bar.com', password: 'pass123' };
+
+  it('should log the user in', async () => {
+    await supertest(app)
+      .post('/api/auth/signup')
+      .set('Content-type', 'application/json')
+      .send(params);
+
+    await supertest(app)
+      .post('/api/auth/login')
+      .set('Content-type', 'application/json')
+      .send(params)
+      .expect(200)
+      .then(response => {
+        const { token } = response.body;
+        expect(token).toBeDefined();
+        expect(token).toHaveLength(224);
+      });
+  });
+
+  it('should return an error if the user is not found.', async () => {
+    await supertest(app)
+      .post('/api/auth/login')
+      .set('Content-type', 'application/json')
+      .send(params)
+      .expect(400)
+      .then(response => {
+        expect(response.body.error).toEqual('Invalid login.');
+      });
+  });
+});
+
+describe('GET /api/auth/verify-token', () => {
+  it('should verify a valid token', async () => {
+    await supertest(app)
+      .post('/api/auth/verify-token')
+      .set('Content-type', 'application/json')
+      .send({ email: 'foo@bar.com', password: 'pass123' })
+      .expect(201)
+      .then(response => {
+        const { token } = response.body;
+        expect(token).toBeDefined();
+        expect(token).toHaveLength(224);
+      });
+  });
 });
