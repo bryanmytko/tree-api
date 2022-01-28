165f0489fc4a07ace8df4ec27535f891fe4d1ea0
Bryan Mytko <bryanmytko@gmail.com>
2022-01-28 06:18:06 -0500

    add GET /api/auth/verify-token test

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const app = require('../../index');
 const User = require('../../models/user');
 
+const { TOKEN_SECRET } = process.env;
 process.env.TEST_SUITE = 'test-auth';
 
 describe('POST /api/auth/signup', () => {
@@ -68,15 +69,24 @@ describe('POST /api/auth/login', () => {
 
 describe('GET /api/auth/verify-token', () => {
   it('should verify a valid token', async () => {
+    const user = await User({ email: 'john@beatles.org', password: 'password' }).save();
+    const token = jwt.sign({ data: user.toObject() }, TOKEN_SECRET, { expiresIn: '24h' });
+
     await supertest(app)
-      .post('/api/auth/verify-token')
+      .get('/api/auth/verify-token')
       .set('Content-type', 'application/json')
-      .send({ email: 'foo@bar.com', password: 'pass123' })
-      .expect(201)
+      .set('authorization', token)
+      .expect(200)
       .then(response => {
-        const { token } = response.body;
-        expect(token).toBeDefined();
-        expect(token).toHaveLength(224);
+        expect(response.body.user.email).toEqual(user.email);
       });
+  });
+
+  it('should return a 403 for an invalid token', async () => {
+    await supertest(app)
+      .get('/api/auth/verify-token')
+      .set('Content-type', 'application/json')
+      .set('authorization', '1234')
+      .expect(403)
   });
 });
