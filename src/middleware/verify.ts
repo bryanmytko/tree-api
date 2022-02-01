b5d4415a50d79cd07bf129cb87b25a598537066a
Bryan Mytko <bryanmytko@gmail.com>
2022-02-01 10:31:01 -0500

    WIP middleware

--- src/middleware/verify.ts
+++ src/middleware/verify.ts
@@ -1,17 +1,17 @@
 import jwt from 'jsonwebtoken';
 import { Response, NextFunction } from 'express';
-import { RequestCustom, JwtPayload } from '.';
+import { Error } from 'mongoose';
 
 const { TOKEN_SECRET } = process.env;
 
-const verify = (req: RequestCustom, res: Response, next: NextFunction) => {
+const verify = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers.authorization;
 
   if(!authorization) {
     return res.status(403).json({ error: 'No auth token provided' });
   }
 
-  jwt.verify(authorization, TOKEN_SECRET, (err: any, value: JwtPayload) => {
+  jwt.verify(authorization, TOKEN_SECRET, (err: Error, value: JwtPayload) => {
     if(err) return res.status(403).json({ error: 'Invalid token' });
     req.user = value.data;
     req.body = { ...req.body, user: value.data }
