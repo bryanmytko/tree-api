dab0a94a68ce3c3899cf48ed9ab17f671298633b
Bryan Mytko <bryanmytko@gmail.com>
2022-02-03 06:47:50 -0500

    Add environment types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,27 @@
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string;
+      TOKEN_SECRET: string;
+      MONGO_URL: string;
+      MONGO_TEST_URL: string;
+    }
+  }
+  
+  interface ENV {
+    NODE_ENV: string | undefined;
+    TOKEN_SECRET: string | undefined;
+    MONGO_URL: string | undefined;
+    MONGO_TEST_URL: string | undefined;
+  }
+  
+  interface Config {
+    NODE_ENV: string;
+    TOKEN_SECRET: string;
+    MONGO_URL: string;
+    MONGO_TEST_URL: string;
+  }
+
   interface JwtPayload {
     data: string;
   }
